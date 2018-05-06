4c702746f4259b16768c5273e0169777296a458b
Ankit Dutta <Ankit Dutta>
2018-05-07 00:27:12 +0530

    Initial express server setup with mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://ankit:ankit@ds215910.mlab.com:15910/devspace'
+}
---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db)
+    .then(() => console.log('MongoDB connected.'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`App runnning on port ${port}`));
