918fd24091ef02ed6ffca5ee154b7b0ef780577e
Ankit Dutta <Ankit Dutta>
2018-05-07 14:41:13 +0530

    Added UserSchema to /model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
