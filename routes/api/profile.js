2dc7bbc8fecfb04acdf0cf6cffd6207a969e17ce
cruxbreaker <geekankit318@gmail.com>
2018-10-14 04:37:33 +0530

    Add Experience and Education POST routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,8 @@ const passport = require('passport');
 
 // Load validation
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
@@ -175,7 +177,83 @@ router.post('/', passport.authenticate('jwt', {
                         new Profile(profileFields).save().then(proile => res.json(profile));
                     });
             }
+        });
+});
+
+//  @route      POST api/profile/experience
+//  @desc       Add experience to profile
+//  @accsess    Private
+router.post('/experience', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateExperienceInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Profile.findOne({
+            user: req.user.id
         })
+        .then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.location,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description,
+            }
+
+            // Add to experince array
+            profile.experience.unshift(newExp);
+
+            profile.save().then(profile => res.json(profile));
+        });
+});
+
+//  @route      POST api/profile/education
+//  @desc       Add education to profile
+//  @accsess    Private
+router.post('/education', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateEducationInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Profile.findOne({
+            user: req.user.id
+        })
+        .then(profile => {
+            const newEdu = {
+                school: req.body.school,
+                degree: req.body.degree,
+                fieldofstudy: req.body.fieldofstudy,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description,
+            }
+
+            // Add to experince array
+            profile.education.unshift(newEdu);
+
+            profile.save().then(profile => res.json(profile));
+        });
 });
 
 module.exports = router;
---
+++ validation/education.js
@@ -0,0 +1,32 @@
+const validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = validateEducationInput = (data) => {
+    let errors = {};
+
+    data.school = !isEmpty(data.school) ? data.school : '';
+    data.degree = !isEmpty(data.degree) ? data.degree : '';
+    data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : '';
+    data.from = !isEmpty(data.from) ? data.from : '';
+
+    if (validator.isEmpty(data.school)) {
+        errors.school = 'School field is required';
+    }
+
+    if (validator.isEmpty(data.degree)) {
+        errors.degree = 'Degree field is required';
+    }
+
+    if (validator.isEmpty(data.from)) {
+        errors.from = 'From date field is required';
+    }
+
+    if (validator.isEmpty(data.fieldofstudy)) {
+        errors.fieldofstudy = 'Field of study field is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
